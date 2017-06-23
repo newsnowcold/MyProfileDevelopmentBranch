3338fe6c256b5dcb9e44d374d97c4bc78e201427
Darrel Abello <netzon.darrel@gmail.com>
2017-06-23 15:43:50 +0800

    initial code drop

---
+++ src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,22 @@
+import { Component, AfterViewInit, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ContactUsComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+
+  }
+}
---
+++ src/app/my-services/my-services.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-my-services',
+  templateUrl: './my-services.component.html',
+  styleUrls: ['./my-services.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class MyServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
